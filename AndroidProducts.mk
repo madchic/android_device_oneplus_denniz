2c92c160cc22790b858f970bced02c850594023f
frxncyyy <teamassistenza37@gmail.com>
2023-03-28 13:00:11 +0200

    denniz: Switch to Lineage

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -15,9 +15,9 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/arrow_denniz.mk
+    $(LOCAL_DIR)/lineage_denniz.mk
 
 COMMON_LUNCH_CHOICES := \
-    arrow_denniz-eng \
-    arrow_denniz-userdebug \
-    arrow_denniz-user
+    lineage_denniz-eng \
+    lineage_denniz-userdebug \
+    lineage_denniz-user
--- lineage_denniz.mk
+++ lineage_denniz.mk
@@ -21,28 +21,20 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 # Inherit from device makefile
 $(call inherit-product, device/oneplus/denniz/device.mk)
 
-# Inherit some common ArrowOS stuff.
-$(call inherit-product, vendor/arrow/config/common.mk)
+# Inherit some common Lineage stuff.
+$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 
-# ArrowOS Stuff
-IS_PHONE := true
+# Lineage Stuff
 TARGET_BOOT_ANIMATION_RES := 1080
-TARGET_SUPPORTS_QUICK_TAP := true
-TARGET_SUPPORTS_CALL_RECORDING := true
-FORCE_AOSP_DIALER := true
 
 # Device identifier. This must come after all inclusions.
-PRODUCT_NAME := aosp_denniz
+PRODUCT_NAME := lineage_denniz
 PRODUCT_DEVICE := denniz
 PRODUCT_BRAND := OnePlus
 PRODUCT_MODEL := DN2103
 PRODUCT_MANUFACTURER := OnePlus
 
 # Gapps
-TARGET_GAPPS_ARCH := arm64
-TARGET_BOOT_ANIMATION_RES := 1080
-TARGET_SUPPORTS_QUICK_TAP := true
-
 PRODUCT_GMS_CLIENTID_BASE := android-oneplus
 
 # Build info
